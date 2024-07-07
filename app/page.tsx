1118e694d597ca08f25d7491ba7d1b013f2a5bd0
Chase Peterson <cep.piano@gmail.com>
2024-07-06 18:56:02 -0700

    Built first version of site

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,194 @@
+"use client";
 import Image from "next/image";
+
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
+
+import { motion } from "framer-motion";
+
+const textBlockStyle =
+  "flex flex-grow flex-col justify-center space-y-4 overflow-hidden rounded-md bg-gradient-to-br bg-fixed p-5 font-raleway text-xl leading-8 shadow-inner-xl";
+
+const headingStyle = "font-source-serif text-3xl font-medium tracking-tight";
+
+const imageStyle =
+  "aspect-square h-fit overflow-hidden rounded-md bg-blue-300 shadow-xl hover:cursor-pointer hover:-translate-y-1 transition-transform size-fit object-cover";
+
+const linkStyle =
+  "font-bold transition-colors text-blue-900 hover:text-purple-900";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="mx-4 my-24 space-y-24">
+      <Hero />
+      <MachineShopInterface />
+      <MachineShopInterface />
+      <MachineShopInterface />
+      <MachineShopInterface />
+      <MachineShopInterface />
+      <MachineShopInterface />
+    </main>
+  );
+}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+function Hero() {
+  return (
+    <motion.section
+      initial={{ y: -50, opacity: 0, scale: 0.8 }}
+      whileInView={{ y: 0, opacity: 1, scale: 1 }}
+      viewport={{ once: true }}
+      transition={{ ease: "easeOut" }}
+      className="mx-auto grid max-w-[960px] grid-cols-3 gap-9"
+    >
+      {/* Picture of Chase */}
+      <div>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/images/fjordpic.jpg"
+          alt="Picture of Chase Peterson"
+          width={1000}
+          height={1000}
+          className={`${imageStyle} hover:translate-y-0 hover:cursor-default`}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+      {/* Name & Text */}
+      <div className="col-span-2 flex flex-col gap-4">
+        <h1 className={headingStyle}>Chase Peterson Portfolio</h1>
+        <div className={`${textBlockStyle} bg-blue-300`}>
+          <p>
+            {"Hi! I'm a software engineer studying "}
+            <b className="font-bold">Computer Science at UC San Diego</b>
+            {". I have years of experience as a "}
+            <b className="font-bold">full-stack developer</b>
+            {", with an emphasis on "}
+            <b className="font-bold">front-end development and UI/UX design</b>.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <p>Below you can find some of my projects and experience.</p>
+        </div>
       </div>
-    </main>
+    </motion.section>
+  );
+}
+
+function MachineShopInterface() {
+  return (
+    <PhotoProvider>
+      <motion.section
+        initial={{ y: -50, opacity: 0, scale: 0.8 }}
+        whileInView={{ y: 0, opacity: 1, scale: 1 }}
+        viewport={{ once: true }}
+        transition={{ ease: "easeOut" }}
+        className="mx-auto max-w-[960px]"
+      >
+        <h1 className={`${headingStyle} mb-4`}>
+          <a
+            href="http://shop.origingolf.com"
+            className="text-yellow-800 transition-colors hover:text-yellow-950"
+            target="_blank"
+          >
+            Machine Shop Interface
+          </a>
+          {" ~ "}
+          Origin Golf
+        </h1>
+        {/* Grid */}
+        <div className="grid grid-cols-3 gap-9">
+          <div className="col-span-2 flex flex-col gap-4">
+            <div className={`${textBlockStyle} bg-yellow-300`}>
+              <p>
+                At <b>Origin Golf</b>, I developed an interface to{" "}
+                <b>track all active jobs</b> being run in the machine shop. The
+                page has a simple and easy-to-use{" "}
+                <a
+                  href="http://shop.origingolf.com/"
+                  className={linkStyle}
+                  target="_blank"
+                >
+                  front-end interface
+                </a>{" "}
+                with a <b>password-locked edit system</b> for administrators.
+              </p>
+              <p>
+                Along with allowing users to <b>easily view and edit jobs</b>,
+                the website also <b>tracks all job history over time</b>. This
+                means the full shop history is viewable in a cleanly-designed{" "}
+                <a
+                  href="http://shop.origingolf.com/history"
+                  className={linkStyle}
+                  target="_blank"
+                >
+                  history log
+                </a>
+                , as well as a{" "}
+                <a
+                  href="http://shop.origingolf.com/moment"
+                  className={linkStyle}
+                  target="_blank"
+                >
+                  moment history
+                </a>{" "}
+                page that shows the shop's exact state at any point in the past.
+              </p>
+              <p>
+                All pages are also built with <b>responsive design</b>, and look
+                clean on all devices.
+              </p>
+              <p>
+                This tool was primarily built with{" "}
+                <b>React (Next.js), JavaScript, and HTML/CSS</b>, with the
+                back-end built on <b>Node.js and SQL</b>.
+              </p>
+            </div>
+          </div>
+          {/* Pictures */}
+          <div className="flex-1 space-y-9">
+            <PhotoView src="/images/machineshop/shop.png">
+              <Image
+                src="/images/machineshop/shop.png"
+                alt="Picture of Chase Peterson"
+                width={1000}
+                height={1000}
+                className={imageStyle}
+              />
+            </PhotoView>
+            <PhotoView src="/images/machineshop/saw.png">
+              <Image
+                src="/images/machineshop/saw.png"
+                alt="Picture of Chase Peterson"
+                width={1000}
+                height={1000}
+                className={imageStyle}
+              />
+            </PhotoView>
+          </div>
+          <PhotoView src="/images/machineshop/moment.png">
+            <Image
+              src="/images/machineshop/moment.png"
+              alt="Picture of Chase Peterson"
+              width={1000}
+              height={1000}
+              className={imageStyle}
+            />
+          </PhotoView>
+          <PhotoView src="/images/machineshop/history.png">
+            <Image
+              src="/images/machineshop/history.png"
+              alt="Picture of Chase Peterson"
+              width={1000}
+              height={1000}
+              className={imageStyle}
+            />
+          </PhotoView>
+          <PhotoView src="/images/machineshop/historyfilter.png">
+            <Image
+              src="/images/machineshop/historyfilter.png"
+              alt="Picture of Chase Peterson"
+              width={1000}
+              height={1000}
+              className={imageStyle}
+            />
+          </PhotoView>
+        </div>
+      </motion.section>
+    </PhotoProvider>
   );
 }
