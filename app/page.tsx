2a3b62d0af3d8cdef076c281d75bddec6ea24754
Chase Peterson <cep.piano@gmail.com>
2024-08-22 17:20:48 -1000

    Fixed VGDC link

--- app/page.tsx
+++ app/page.tsx
@@ -573,7 +573,11 @@ function VGDC() {
               </p>
               <p>
                 As technology director, I built a{" "}
-                <a href="vgdc.dev" className={linkStyle} target="_blank">
+                <a
+                  href="https://vgdc.dev"
+                  className={linkStyle}
+                  target="_blank"
+                >
                   new website
                 </a>{" "}
                 from the ground up using <b>Next.js</b>, similar to the above
